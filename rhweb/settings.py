d9b61242a82e9284e174d1ce8667553d5fb091c6
Shane <shane@shanerice.me>
2018-02-19 16:23:08 -0800

    Adding MySQL

--- rhweb/settings.py
+++ rhweb/settings.py
@@ -31,24 +31,17 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-if 'sqlite_db' in CONFIG['django']:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': CONFIG['database']['db_name'],
+        'USER': CONFIG['database']['db_user'],
+        'PASSWORD': CONFIG['database']['db_pass'],
+        'HOST': CONFIG['database']['db_host'],
+        'PORT': CONFIG['database']['db_port'],
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': CONFIG['database']['db_name'],
-            'USER': CONFIG['database']['db_user'],
-            'PASSWORD': CONFIG['database']['db_pass'],
-            'HOST': CONFIG['database']['db_host'],
-            'PORT': CONFIG['database']['db_port'],
-        }
-    }
+}
 
 LOGGING = {
     'version': 1,
